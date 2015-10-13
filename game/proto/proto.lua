b4a68848a3b9969390ad7c4cfd8b65f2d9ff7f47
li <chris@lis-Mac-mini.local>
2015-10-13 19:39:08 +0800

    add pb util

---
+++ game/proto/proto.lua
@@ -0,0 +1,21 @@
+local protoparser = require "protoparser"
+-- preload, execute one time
+protoparser.load("proto.addressbook")
+
+local proto = {}
+
+local type2name = {[1] = "register", [2] = "login"}
+local name2type = {}
+for k, v in pairs(type2name) do
+	name2type[v] = k
+end
+
+function proto.getname(type)
+	return type2name[type]
+end
+
+function proto.gettype(name)
+	return name2type[name]
+end
+
+return proto
