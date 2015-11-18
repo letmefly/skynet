8e6e75cf2e5b8f20b52614a82df8a04e670f584a
li <chris@lis-Mac-mini.local>
2015-11-18 17:20:31 +0800

    add errno json config, proto2type json config for client C#

--- gameserver/proto/prototext.lua
+++ gameserver/proto/prototext.lua
@@ -1,3 +1,5 @@
+local cjson = require "cjson"
+
 local prototext = [[
 
 message handshake {
@@ -24,12 +26,40 @@ message user_login_ack {
 
 ]]
 
-PROTO_TYPE2NAME = {
-	[1] = "user_register",
-	[2] = "user_register_ack", 
-	[3] = "user_login", 
-	[4] = "user_login_ack",
-	[5] = "handshake"
+local type2name_json = [[
+
+{
+	"1": "user_register",
+	"2": "user_register_ack", 
+	"3": "user_login", 
+	"4": "user_login_ack",
+	"5": "handshake"	
 }
+
+]]
+
+local errno2desp_json = [[
+
+{
+	"1000": "用户已存在",
+	"1001": "数据库错误"
+}
+
+]]
+
+PROTO_TYPE2NAME = {}
+
+local type2name = cjson.decode(type2name_json)
+for k, v in pairs(type2name) do
+	PROTO_TYPE2NAME[tonumber(k)] = v
+end
+
+-- PROTO_TYPE2NAME = {
+-- 	[1] = "user_register",
+-- 	[2] = "user_register_ack", 
+-- 	[3] = "user_login", 
+-- 	[4] = "user_login_ack",
+-- 	[5] = "handshake"
+-- }
 
 return prototext
