8da8542dfe9902b017b643a38233070dd56c3e6d
li <chris@lis-Mac-mini.local>
2015-12-02 14:28:33 +0800

    game result api ok

--- gameserver/agent_s/shopdata.lua
+++ gameserver/agent_s/shopdata.lua
@@ -1,4 +1,23 @@
+local cjson = require "cjson"
+local skynet = require "skynet"
+local userdata = require "agent_s.userdata"
+
 local shopdata = {data = {}}
+
+function shopdata:load()
+	-- local userid = userdata:get("user_id")
+	-- local result = skynet.call("db_s", "lua", "select_characters", {user_id = userid})
+	-- if result.errno == 0 and next(result.data) ~= nil then
+	-- 	self.data = result.data
+	-- end
+	-- -- print(cjson.encode(self.data))
+	-- return result
+end
+
+function shopdata:save()
+	-- local result = skynet.call("db_s", "lua", "update_characters", self.data)
+	-- return result
+end
 
 
 
