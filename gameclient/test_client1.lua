394ea8aad150bfc1783f6b5b4b0ac6fab3afa39a
root <root@fatiao.(none)>
2016-11-23 15:27:25 +0800

    2016-11-23 2

---
+++ gameclient/test_client1.lua
@@ -0,0 +1,23 @@
+package.path = "lualib/?.lua;gameclient/?.lua;gameclient/?/?.lua"
+local util = require "util"
+local socket = require "clientsocket"
+
+local fd = assert(socket.connect("127.0.0.1", 8888))
+--util.reg("gameLogin_ack", function(msg) end)
+
+util.sendmsg(fd, "gameLogin", {userId = "chris1", authCode = "123456", version = 1})
+
+while true do
+	local msgname, msg = util.recvmsg(fd)
+	if msgname == "gameLogin_ack" then
+		util.sendmsg(fd, "createRoom", {roomType = 1})
+	elseif msgname == "createRoom_ack" then
+		util.sendmsg(fd, "joinRoom", {roomNo = msg.roomNo})
+	elseif msgname == "joinRoom_ack" then
+		util.sendmsg(fd, "getReady", {status = 1})
+	elseif msgname == "getReady_ntf" then
+	elseif msgname == "whoGrabLandlord_ntf" then
+		util.sendmsg(fd, "grabLandlord", {grabAction = 2})
+	end
+end
+
---
+++ gameclient/test_client2.lua
@@ -0,0 +1,19 @@
+package.path = "lualib/?.lua;gameclient/?.lua;gameclient/?/?.lua"
+local util = require "util"
+local socket = require "clientsocket"
+
+local fd = assert(socket.connect("127.0.0.1", 8888))
+--util.reg("gameLogin_ack", function(msg) end)
+
+util.sendmsg(fd, "gameLogin", {userId = "chris2", authCode = "123456", version = 1})
+
+while true do
+	local msgname, msg = util.recvmsg(fd)
+	if msgname == "gameLogin_ack" then
+		util.sendmsg(fd, "joinRoom", {roomNo = 100001})
+	elseif msgname == "joinRoom_ack" then
+		util.sendmsg(fd, "getReady", {status = 1})
+	elseif msgname == "getReady_ntf" then
+
+	end
+end
